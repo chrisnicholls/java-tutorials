910586ae735d7b004e43edbe6392aeb05d5d7f40
cnicholls <cnicholls@salesforce.com>
2015-04-14 14:44:20 -0300

    better do-while example

--- src/org/code/tutorials/flow/DoWhile.java
+++ src/org/code/tutorials/flow/DoWhile.java
@@ -7,11 +7,20 @@ public class DoWhile
 	{
 		int count = 10;
 		
+		while(count > 10 && count < 20)
+		{
+			System.out.println("In the while loop!");
+			count++;
+		}
+		
+		System.out.println("Past the while loop, count = " + count);
+		
+		//we always do at least one iteration of a do-while loop
 		do
 		{
 			System.out.println("count = " + count);
 			count++;
-		} while(count < 10);
+		} while(count > 10 && count < 20);
 	}
 
 }
